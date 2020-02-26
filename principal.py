616cd85ce4828c4fb3a4706ebc4e512288ef80fa
pablogonhi <pablogonhi@gmail.com>
2020-02-26 13:08:09 +0100

    version para cualquier n

--- principal.py
+++ principal.py
@@ -5,21 +5,20 @@ Created on Wed Feb 26 11:40:19 2020
 @author: pablo
 """
 import random
-n=10
 
 def aleatorio(n):
     lista=[]
     for i in range(n):
-        a=random.randrange(0,10,1)
+        a=random.randrange(0,n,1)
         while a in lista:
-            a=random.randrange(0,10,1)
+            a=random.randrange(0,n,1)
         lista.append(a)
     return lista
             
 def invertida(n):
     lista=[]
     for i in range(n):
-            lista.append(9-i)
+            lista.append(n-1-i)
     return lista
             
 def quasiordenada(n):
@@ -34,7 +33,7 @@ def alterna(n):
         if i%2==0:
             lista.append(int(i/2))
         else:
-            lista.append(9-int((i-1)/2))
+            lista.append(n-1-int((i-1)/2))
     return lista
         
 #n variables
@@ -45,6 +44,7 @@ def alterna(n):
 
 
 if __name__=="__main__":
+    n=20
     print(aleatorio(n))
     print(invertida(n))
     print(quasiordenada(n))
